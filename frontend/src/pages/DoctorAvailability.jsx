782306ff7bde5baa0121ed20b95c0c102e3d2874
Ashwin-singh-chauhan <chouhanashwin61@gmail.com>
2026-08-23 20:21:18 +0530

    Update DoctorAvailability.jsx

--- frontend/src/pages/DoctorAvailability.jsx
+++ frontend/src/pages/DoctorAvailability.jsx
@@ -5,6 +5,7 @@ const API_URL =
   import.meta.env.VITE_API_URL ||
   "https://healthcare-appointment-manager-nlir.onrender.com";
 
+
 function DoctorAvailability() {
 
   const navigate = useNavigate();
